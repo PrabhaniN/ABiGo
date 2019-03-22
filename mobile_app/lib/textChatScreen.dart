cb2a3e7a11821a1938e75abe7af1856d3be82028
Prabhani Nanayakkara <prabhaninanayakkara1907@gmail.com>
2019-03-23 00:50:17 +0530

    Updated chat screen file
    added typing field and messages display area

--- mobile_app/lib/textChatScreen.dart
+++ mobile_app/lib/textChatScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'chatMessage.dart';
 
 class ChatScreen extends StatefulWidget {
   @override
@@ -6,12 +7,72 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final List<ChatMessage> _messages = <ChatMessage>[];
+  bool _isComposing =false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        // leading: _buildStack(),
         title: Text('Name'),
+      ),
+      body: new Container(
+        child: new Column(
+          children: <Widget>[
+            Flexible(
+              child: new ListView.builder(
+                padding: EdgeInsets.all(8.0),
+                itemBuilder: (_, int index) => _messages[index],
+                itemCount: _messages.length,
+                reverse: true,
+              ),
+            ),
+            Divider(height: 1.0),
+            Container(
+              // decoration: BoxDecoration(
+                // color: Theme.of(context).cardColor,
+              // ),
+              child: _buildTextComposer(),
+            ),
+          ],
+        ),
       ),
     );
   }
+  Widget _buildTextComposer() {
+    return IconTheme(
+      data: IconThemeData(
+        color: Theme.of(context).accentColor
+      ),
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: <Widget>[
+            Flexible(
+              child: TextField(
+                controller: TextEditingController(),
+                onChanged: (String text) {
+                  setState(() {
+                    _isComposing = text.length > 0;
+                  });
+                },
+                decoration: InputDecoration.collapsed(
+                  hintText: 'Send Message'
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+  // Widget _buildStack() => Stack(
+    // alignment: const Alignment(10.0, 10.0),
+    // children: [
+      // CircleAvatar(
+        // backgroundImage: AssetImage('images/panda.jpeg'),
+        // radius: 25.0, 
+      // ),
+    // ],
+  // );
 }
