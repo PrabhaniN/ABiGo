b56303135100d79713a279451f36605aebff4304
Prabhani Nanayakkara <prabhaninanayakkara1907@gmail.com>
2019-03-23 11:23:37 +0530

    updated sign up form

--- mobile_app/lib/signup.dart
+++ mobile_app/lib/signup.dart
@@ -7,21 +7,31 @@ class SignupPage extends StatefulWidget {
 }
 
 class _SignupPageState extends State<SignupPage> {
+  final _firstnameController =TextEditingController();
+  final _lastnameController =TextEditingController();
   final _usernameController =TextEditingController();
+  final _phonenumberController =TextEditingController();
   final _formKey = GlobalKey<FormState>();
+  bool isCheckedDeaf = false;
+  bool isCheckedbBlind = false;
+  bool isCheckedMute = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Sign-Up'),
+      ),
       body: Form(
         key: _formKey,
         child: Column(
           children: <Widget>[
             SizedBox(
-              height: 50.0,
+              height: 10.0,
             ),
             TextFormField(
-              controller: _usernameController,
+              controller: _firstnameController,
               validator: (value) {
                 if(value.isEmpty) {
                   return 'Enter a valid name';
@@ -30,15 +40,15 @@ class _SignupPageState extends State<SignupPage> {
               decoration: InputDecoration(
                 hintText: 'Enter your First name',
                 filled: true,
-                contentPadding: EdgeInsets.all(20.0)
+                contentPadding: EdgeInsets.all(10.0)
               ),
             ),
             SizedBox(
-              height: 25.0,
+              height: 10.0,
             ),
 
             TextFormField(
-              controller: _usernameController,
+              controller: _lastnameController,
               validator: (value) {
                 if(value.isEmpty) {
                   return 'Enter a valid name';
@@ -47,11 +57,11 @@ class _SignupPageState extends State<SignupPage> {
               decoration: InputDecoration(
                 hintText: 'Enter your Last name',
                 filled: true,
-                contentPadding: EdgeInsets.all(20.0)
+                contentPadding: EdgeInsets.all(10.0)
               ),
             ),
             SizedBox(
-              height: 25.0,
+              height: 10.0,
             ),
 
             TextFormField(
@@ -64,22 +74,99 @@ class _SignupPageState extends State<SignupPage> {
               decoration: InputDecoration(
                 hintText: 'Enter your username',
                 filled: true,
-                contentPadding: EdgeInsets.all(20.0)
+                contentPadding: EdgeInsets.all(10.0)
               ),
             ),
             SizedBox(
-              height: 25.0,
+              height: 10.0,
             ),
+            TextFormField(
+              controller: _phonenumberController,
+              validator: (value) {
+                if(value.isEmpty) {
+                  return 'Enter a valid phone number';
+                }
+              },
+              decoration: InputDecoration(
+                hintText: 'Enter Guardian\'s phone',
+                filled: true,
+                contentPadding: EdgeInsets.all(10.0)
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Container(
+              child: Column(
+                children: <Widget>[
+                  SizedBox(
+                    height: 10.0,
+                    child: Text('Disability'),
+                  ),
+                  SizedBox(
+                    height: 8.0,
+                  ),
+                  new Row(
+                    children: <Widget>[
+                      new Checkbox(
+                        value: isCheckedbBlind,
+                        onChanged: (value) {
+                          setState(() {
+                            isCheckedbBlind = value;
+                          });
+                        },
+                      ),
+                      new Text('Blind'),
+                      SizedBox(
+                        width: 60.0,
+                      ),
+
+                      new Checkbox(
+                        value: isCheckedMute,
+                        onChanged: (value) {
+                          setState(() {
+                            isCheckedMute = value;
+                          });
+                        },
+                      ),
+                      new Text('Mute'),
+                      SizedBox(
+                        width: 60.0,
+                      ),
+                      
+                      new Checkbox(
+                        value: isCheckedDeaf,
+                        onChanged: (value) {
+                          setState(() {
+                            isCheckedDeaf = value;
+                          });
+                        },
+                      ),
+                      new Text('Deaf'),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            // SizedBox(
+              // height: 10,
+            // ),
             ButtonTheme(
-              height: 50.0,
-              minWidth: 150.0,
+              height: 35.0,
+              minWidth: 105.0,
               buttonColor: Colors.blue[900],
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(8.0)
               ),
               child: RaisedButton(
                 textColor: Colors.white,
-                child: Text('Continue'),
+                child: Text(
+                  'Sign Up',
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontFamily: 'Roboto'
+                  ),
+                ),
                 onPressed: (){
                   if(_formKey.currentState.validate()){
                     Navigator.push(context, MaterialPageRoute(builder: (context) => ChatPage()));
