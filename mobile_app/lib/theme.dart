903ad404780b87dcb829b6d6ac925e48a46dec68
Prabhani Nanayakkara <prabhaninanayakkara1907@gmail.com>
2019-05-12 12:42:02 +0530

    Updated theme

--- mobile_app/lib/theme.dart
+++ mobile_app/lib/theme.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 
-final ThemeData IOSTheme = new ThemeData(
-  primarySwatch: Colors.blue[900],
-  primaryColor: Colors.grey,
-  primaryColorBrightness: Brightness.light
-);
+// final ThemeData IOSTheme = new ThemeData(
+//   primarySwatch: Colors.blue[900],
+//   primaryColor: Colors.grey,
+//   primaryColorBrightness: Brightness.light
+// );
 
-final ThemeData DefaultTheme = new ThemeData(
-  primarySwatch: Colors.blue[900],
-  accentColor: Colors.blue[900]
-);
+// final ThemeData DefaultTheme = new ThemeData(
+//   primarySwatch: Colors.blue[900],
+//   accentColor: Colors.blue[900]
+// );
+
+class ThemeColors {
+  static const Color primary =Color.fromRGBO(36, 159, 217, 100.0);
+}
