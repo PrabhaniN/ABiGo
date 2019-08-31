8cd24415bcc0a90e3c7f48c80f121ea44a849e65
Prabhani Nanayakkara <prabhaninanayakkara1907@gmail.com>
2019-08-31 06:41:12 +0530

    UI Improvements

--- abigo_mobile/lib/screens/intro/intro_screen.dart
+++ abigo_mobile/lib/screens/intro/intro_screen.dart
@@ -19,40 +19,53 @@ class IntroScreenState extends State<IntroScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Center(
-            child: Text(
-              'What you can do?',
-              style: TextStyle(fontSize: 20.0),
+      appBar: AppBar(
+        title: Center(
+          child: Text(
+            'What you can do?',
+            style: TextStyle(fontSize: 20.0),
+          ),
+        ),
+        backgroundColor: Color.fromRGBO(35, 153, 209, 1),
+        // bottom: ,
+      ),
+      body: GridView.count(
+        padding: EdgeInsets.all(10.0),
+        crossAxisCount: 1,
+        childAspectRatio: 5.0 / 2.0,
+        children: <Widget>[
+          Card(
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(top: 20.0, left: 20.0),
+                ),
+                ListTile(
+                  leading: Image.asset(
+                    'assets/images/voice-to-text.png',
+                    // width: 200.0,
+                  ),
+                  title: Text('Communication'),
+                  onTap: _gotoLoginScreen,
+                )
+              ],
             ),
           ),
-          backgroundColor: Color.fromRGBO(35, 153, 209, 1),
-          // bottom: ,
+        ],
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _gotoLoginScreen,
+        label: Text(
+          'Next',
+          style: TextStyle(
+            fontSize: 20.0
+          ),
         ),
-        body: GridView.count(
-          padding: EdgeInsets.all(10.0),
-          crossAxisCount: 1,
-          childAspectRatio: 5.0 / 2.0,
-          children: <Widget>[
-            Card(
-              child: Column(
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.only(top: 20.0, left: 20.0),
-                  ),
-                  ListTile(
-                    leading: Image.asset(
-                      'assets/images/voice-to-text.png',
-                      // width: 200.0,
-                    ),
-                    title: Text('Communication'),
-                    onTap: _gotoLoginScreen,
-                  )
-                ],
-              ),
-            ),
-          ],
-        ));
+        icon: Icon(Icons.navigate_next, size: 30.0,),
+        backgroundColor: Color.fromRGBO(35, 153, 209, 1),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+    );
   }
 
   void _gotoLoginScreen() {
